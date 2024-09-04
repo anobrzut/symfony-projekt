c1fa17c21493a2405c33da69800d3852517dd5b6
anobrzut <anna.obrzut@onet.com.pl>
2024-09-04 12:36:25 +0200

    Update

--- app/migrations/Version20240904103412.php
+++ app/migrations/Version20240904103412.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240831182056 extends AbstractMigration
+final class Version20240904103412 extends AbstractMigration
 {
     public function getDescription(): string
     {
