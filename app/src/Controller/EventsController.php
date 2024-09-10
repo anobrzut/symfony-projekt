a4279b54f63b471295caf01d9c735049e2cea027
anobrzut <anna.obrzut@onet.com.pl>
2024-09-10 18:10:37 +0200

    Test

--- app/src/Controller/EventsController.php
+++ app/src/Controller/EventsController.php
@@ -17,164 +17,164 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 /**
- * Class EventsController.
- */
+* Class EventsController.
+*/
 #[Route('/events')]
 class EventsController extends AbstractController
 {
-    /**
-     * Constructor.
-     *
-     * @param EventsServiceInterface $eventsService Events service
-     * @param TranslatorInterface    $translator    Translator
-     */
-    public function __construct(
-        private readonly EventsServiceInterface $eventsService,
-        private readonly TranslatorInterface $translator
-    ) {
-    }
+/**
+* Constructor.
+*
+* @param EventsServiceInterface $eventsService Events service
+* @param TranslatorInterface    $translator    Translator
+*/
+public function __construct(
+private readonly EventsServiceInterface $eventsService,
+private readonly TranslatorInterface $translator
+) {
+}
 
-    /**
-     * Index action.
-     *
-     * @param Request $request HTTP request
-     *
-     * @return Response HTTP response
-     */
-    #[Route(name: 'event_index', methods: 'GET')]
-    public function index(Request $request): Response
-    {
-        $page = $request->query->getInt('page', 1);
-        $user = $this->getUser(); // Get the current authenticated user
+/**
+* Index action.
+*
+* @param Request $request HTTP request
+*
+* @return Response HTTP response
+*/
+#[Route(name: 'event_index', methods: 'GET')]
+public function index(Request $request): Response
+{
+$page = $request->query->getInt('page', 1);
+$user = $this->getUser(); // Get the current authenticated user
 
-        $pagination = $this->eventsService->getPaginatedList($page, $user);
+$pagination = $this->eventsService->getPaginatedList($page, $user);
 
-        return $this->render('events/index.html.twig', ['pagination' => $pagination]);
-    }
+return $this->render('events/index.html.twig', ['pagination' => $pagination]);
+}
 
-    /**
-     * Show action.
-     *
-     * @param Events $event Event entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route('/{id}', name: 'event_show', requirements: ['id' => '[1-9]\d*'], methods: 'GET')]
-    #[IsGranted('VIEW', subject: 'event')]
-    public function show(Events $event): Response
-    {
-        return $this->render('events/show.html.twig', ['event' => $event]);
-    }
+/**
+* Show action.
+*
+* @param Events $event Event entity
+*
+* @return Response HTTP response
+*/
+#[Route('/{id}', name: 'event_show', requirements: ['id' => '[1-9]\d*'], methods: 'GET')]
+#[IsGranted('VIEW', subject: 'event')]
+public function show(Events $event): Response
+{
+return $this->render('events/show.html.twig', ['event' => $event]);
+}
 
-    /**
-     * Create action.
-     *
-     * @param Request $request HTTP request
-     *
-     * @return Response HTTP response
-     */
-    #[Route('/create', name: 'event_create', methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
-    {
-        /** @var User $user */
-        $user = $this->getUser();
-        $event = new Events();
-        $event->setAuthor($user);
+/**
+* Create action.
+*
+* @param Request $request HTTP request
+*
+* @return Response HTTP response
+*/
+#[Route('/create', name: 'event_create', methods: ['GET', 'POST'])]
+public function create(Request $request): Response
+{
+/** @var User $user */
+$user = $this->getUser();
+$event = new Events();
+$event->setAuthor($user);
 
-        $form = $this->createForm(EventsType::class, $event);
-        $form->handleRequest($request);
+$form = $this->createForm(EventsType::class, $event);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->eventsService->save($event);
+if ($form->isSubmitted() && $form->isValid()) {
+$this->eventsService->save($event);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.created_successfully')
-            );
+$this->addFlash(
+'success',
+$this->translator->trans('message.created_successfully')
+);
 
-            return $this->redirectToRoute('event_index');
-        }
+return $this->redirectToRoute('event_index');
+}
 
-        return $this->render('events/create.html.twig', ['form' => $form->createView()]);
-    }
+return $this->render('events/create.html.twig', ['form' => $form->createView()]);
+}
 
-    /**
-     * Edit action.
-     *
-     * @param Request $request HTTP request
-     * @param Events  $event   Event entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route('/{id}/edit', name: 'event_edit', requirements: ['id' => '[1-9]\d*'], methods: ['GET', 'PUT'])]
-    #[IsGranted('EDIT', subject: 'event')]
-    public function edit(Request $request, Events $event): Response
-    {
-        $form = $this->createForm(
-            EventsType::class,
-            $event,
-            [
-                'method' => 'PUT',
-                'action' => $this->generateUrl('event_edit', ['id' => $event->getId()]),
-            ]
-        );
-        $form->handleRequest($request);
+/**
+* Edit action.
+*
+* @param Request $request HTTP request
+* @param Events  $event   Event entity
+*
+* @return Response HTTP response
+*/
+#[Route('/{id}/edit', name: 'event_edit', requirements: ['id' => '[1-9]\d*'], methods: ['GET', 'PUT'])]
+#[IsGranted('EDIT', subject: 'event')]
+public function edit(Request $request, Events $event): Response
+{
+$form = $this->createForm(
+EventsType::class,
+$event,
+[
+'method' => 'PUT',
+'action' => $this->generateUrl('event_edit', ['id' => $event->getId()]),
+]
+);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->eventsService->save($event);
+if ($form->isSubmitted() && $form->isValid()) {
+$this->eventsService->save($event);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.updated_successfully')
-            );
+$this->addFlash(
+'success',
+$this->translator->trans('message.updated_successfully')
+);
 
-            return $this->redirectToRoute('event_index');
-        }
+return $this->redirectToRoute('event_index');
+}
 
-        return $this->render(
-            'events/edit.html.twig',
-            [
-                'form' => $form->createView(),
-                'event' => $event,
-            ]
-        );
-    }
+return $this->render(
+'events/edit.html.twig',
+[
+'form' => $form->createView(),
+'event' => $event,
+]
+);
+}
 
-    /**
-     * Delete action.
-     *
-     * @param Request $request HTTP request
-     * @param Events  $event   Event entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route('/{id}/delete', name: 'event_delete', requirements: ['id' => '[1-9]\d*'], methods: ['GET', 'DELETE'])]
-    #[IsGranted('DELETE', subject: 'event')]
-    public function delete(Request $request, Events $event): Response
-    {
-        $form = $this->createForm(FormType::class, $event, [
-            'method' => 'DELETE',
-            'action' => $this->generateUrl('event_delete', ['id' => $event->getId()]),
-        ]);
-        $form->handleRequest($request);
+/**
+* Delete action.
+*
+* @param Request $request HTTP request
+* @param Events  $event   Event entity
+*
+* @return Response HTTP response
+*/
+#[Route('/{id}/delete', name: 'event_delete', requirements: ['id' => '[1-9]\d*'], methods: ['GET', 'DELETE'])]
+#[IsGranted('DELETE', subject: 'event')]
+public function delete(Request $request, Events $event): Response
+{
+$form = $this->createForm(FormType::class, $event, [
+'method' => 'DELETE',
+'action' => $this->generateUrl('event_delete', ['id' => $event->getId()]),
+]);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->eventsService->delete($event);
+if ($form->isSubmitted() && $form->isValid()) {
+$this->eventsService->delete($event);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.deleted_successfully')
-            );
+$this->addFlash(
+'success',
+$this->translator->trans('message.deleted_successfully')
+);
 
-            return $this->redirectToRoute('event_index');
-        }
+return $this->redirectToRoute('event_index');
+}
 
-        return $this->render(
-            'events/delete.html.twig',
-            [
-                'form' => $form->createView(),
-                'event' => $event,
-            ]
-        );
-    }
+return $this->render(
+'events/delete.html.twig',
+[
+'form' => $form->createView(),
+'event' => $event,
+]
+);
+}
 }
--- app/src/Repository/EventsRepository.php
+++ app/src/Repository/EventsRepository.php
@@ -13,83 +13,83 @@ use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\NonUniqueResultException;
 
 /**
-* Class EventsRepository.
-*
-* @method Events|null find($id, $lockMode = null, $lockVersion = null)
-* @method Events|null findOneBy(array $criteria, array $orderBy = null)
-* @method Events[]    findAll()
-* @method Events[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-*
-* @extends ServiceEntityRepository<Events>
-    */
-    class EventsRepository extends ServiceEntityRepository
-    {
+ * Class EventsRepository.
+ *
+ * @method Events|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Events|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Events[]    findAll()
+ * @method Events[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends ServiceEntityRepository<Events>
+ */
+class EventsRepository extends ServiceEntityRepository
+{
     public const PAGINATOR_ITEMS_PER_PAGE = 5;
 
     public function __construct(ManagerRegistry $registry)
     {
-    parent::__construct($registry, Events::class);
+        parent::__construct($registry, Events::class);
     }
 
     public function queryAll(): QueryBuilder
     {
-    return $this->getOrCreateQueryBuilder()
-    ->orderBy('events.id', 'ASC');
+        return $this->getOrCreateQueryBuilder()
+            ->orderBy('events.id', 'ASC');
     }
 
     /**
-    * Query events by user.
-    *
-    * @param User $user User entity
-    *
-    * @return QueryBuilder Query builder
-    */
+     * Query events by user.
+     *
+     * @param User $user User entity
+     *
+     * @return QueryBuilder Query builder
+     */
     public function queryByUser(User $user): QueryBuilder
     {
-    $qb = $this->getOrCreateQueryBuilder()
-    ->andWhere('events.author = :user')
-    ->setParameter('user', $user)
-    ->orderBy('events.id', 'ASC');
+        $qb = $this->getOrCreateQueryBuilder()
+            ->andWhere('events.author = :user')
+            ->setParameter('user', $user)
+            ->orderBy('events.id', 'ASC');
 
-    return $qb;
+        return $qb;
     }
 
     private function getOrCreateQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
     {
-    return $queryBuilder ?? $this->createQueryBuilder('events');
+        return $queryBuilder ?? $this->createQueryBuilder('events');
     }
 
     public function save(Events $event): void
     {
-    $entityManager = $this->getEntityManager();
-    $entityManager->persist($event);
-    $entityManager->flush();
+        $entityManager = $this->getEntityManager();
+        $entityManager->persist($event);
+        $entityManager->flush();
     }
 
     public function delete(Events $event): void
     {
-    $entityManager = $this->getEntityManager();
-    $entityManager->remove($event);
-    $entityManager->flush();
+        $entityManager = $this->getEntityManager();
+        $entityManager->remove($event);
+        $entityManager->flush();
     }
 
     /**
-    * Count events by category.
-    *
-    * @param Category $category Category
-    *
-    * @return int Number of events in category
-    *
-    * @throws NoResultException
-    * @throws NonUniqueResultException
-    */
+     * Count events by category.
+     *
+     * @param Category $category Category
+     *
+     * @return int Number of events in category
+     *
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
     public function countByCategory(Category $category): int
     {
-    $qb = $this->getOrCreateQueryBuilder()
-    ->select('COUNT(DISTINCT events.id)')
-    ->where('events.category = :category')
-    ->setParameter('category', $category);
+        $qb = $this->getOrCreateQueryBuilder()
+            ->select('COUNT(DISTINCT events.id)')
+            ->where('events.category = :category')
+            ->setParameter('category', $category);
 
-    return (int) $qb->getQuery()->getSingleScalarResult();
+        return (int) $qb->getQuery()->getSingleScalarResult();
     }
-    }
+}
